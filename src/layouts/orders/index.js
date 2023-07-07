f6f9b35ed16015bd2ff467b87b560f29d545e835
juan <juanxhoo@gmail.com>
2023-07-07 15:03:53 -0500

    some changes

--- src/layouts/orders/index.js
+++ src/layouts/orders/index.js
@@ -15,48 +15,57 @@ Coded by www.creative-tim.com
 
 // @mui material components
 import Grid from "@mui/material/Grid";
-import Divider from "@mui/material/Divider";
-
-// @mui icons
-import FacebookIcon from "@mui/icons-material/Facebook";
-import TwitterIcon from "@mui/icons-material/Twitter";
-import InstagramIcon from "@mui/icons-material/Instagram";
-
+import { Card } from "@mui/material";
 // Material Dashboard 2 React components
 import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
-
+import DataTable from "examples/Tables/DataTable";
 // Material Dashboard 2 React example components
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
-import Footer from "examples/Footer";
-import ProfileInfoCard from "examples/Cards/InfoCards/ProfileInfoCard";
-import ProfilesList from "examples/Lists/ProfilesList";
-import DefaultProjectCard from "examples/Cards/ProjectCards/DefaultProjectCard";
-
-// Overview page components
-import Header from "layouts/profile/components/Header";
-import PlatformSettings from "layouts/profile/components/PlatformSettings";
 
 // Data
-import profilesListData from "layouts/profile/data/profilesListData";
+import providersTableData from "layouts/tables/data/providersTableData";
 
-// Images
-import homeDecor1 from "assets/images/home-decor-1.jpg";
-import homeDecor2 from "assets/images/home-decor-2.jpg";
-import homeDecor3 from "assets/images/home-decor-3.jpg";
-import homeDecor4 from "assets/images/home-decor-4.jpeg";
-import team1 from "assets/images/team-1.jpg";
-import team2 from "assets/images/team-2.jpg";
-import team3 from "assets/images/team-3.jpg";
-import team4 from "assets/images/team-4.jpg";
-
-function Orders() {
+function Providers() {
+  const { columns, rows } = providersTableData();
+  console.log({ rows });
   return (
     <DashboardLayout>
       <DashboardNavbar />
+      <MDBox pt={6} pb={3}>
+        <Grid container spacing={6}>
+          <Grid item xs={12}>
+            <Card>
+              <MDBox
+                mx={2}
+                mt={-3}
+                py={3}
+                px={2}
+                variant="gradient"
+                bgColor="info"
+                borderRadius="lg"
+                coloredShadow="info"
+              >
+                <MDTypography variant="h6" color="white">
+                  Clients Table
+                </MDTypography>
+              </MDBox>
+              <MDBox pt={3}>
+                <DataTable
+                  table={{ columns, rows }}
+                  isSorted={false}
+                  entriesPerPage={false}
+                  showTotalEntries={false}
+                  noEndBorder
+                />
+              </MDBox>
+            </Card>
+          </Grid>
+        </Grid>
+      </MDBox>
     </DashboardLayout>
   );
 }
 
-export default Orders;
+export default Providers;
---
+++ src/layouts/tables/data/ordersTableData.js
@@ -0,0 +1,106 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/function-component-definition */
+/**
+=========================================================
+* Material Dashboard 2 React - v2.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+import MDAvatar from "components/MDAvatar";
+import MDBadge from "components/MDBadge";
+import { useLocation, useNavigate } from "react-router-dom";
+// Images
+import team2 from "assets/images/team-2.jpg";
+
+export default function data() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  console.log(location.pathname);
+  const Provider = ({ image, name, email }) => (
+    <MDBox display="flex" alignItems="center" lineHeight={1}>
+      <MDAvatar src={image} name={name} size="sm" />
+      <MDBox ml={2} lineHeight={1}>
+        <MDTypography display="block" variant="button" fontWeight="medium">
+          {name}
+        </MDTypography>
+        <MDTypography variant="caption">{email}</MDTypography>
+      </MDBox>
+    </MDBox>
+  );
+
+  const Job = ({ title, description }) => (
+    <MDBox lineHeight={1} textAlign="left">
+      <MDTypography display="block" variant="caption" color="text" fontWeight="medium">
+        {title}
+      </MDTypography>
+      <MDTypography variant="caption">{description}</MDTypography>
+    </MDBox>
+  );
+
+  const handleEdit = () => {
+    navigate(location.pathname + "/" + 1);
+  };
+
+  return {
+    columns: [
+      { Header: "order#", accessor: "author", width: "45%", align: "left" },
+      { Header: "REF", accessor: "function", align: "left" },
+      { Header: "Delivery Date", accessor: "status", align: "center" },
+      { Header: "country", accessor: "country", align: "center" },
+      { Header: "Arreglo", accessor: "city", align: "center" },
+      { Header: "Invoice Name", accessor: "email", align: "center" },
+      { Header: "Send To", accessor: "clave", align: "center" },
+      { Header: "Status", accessor: "telefono1", align: "center" },
+      { Header: "Site", accessor: "telefono2", align: "center" },
+      { Header: "Languaje", accessor: "action", align: "center" },
+      { Header: "Total Order", accessor: "action", align: "center" },
+      { Header: "Status Provider", accessor: "action", align: "center" },
+      { Header: "Provider", accessor: "action", align: "center" },
+      { Header: "Client", accessor: "action", align: "center" },
+      { Header: "Invoice", accessor: "action", align: "center" },
+
+    ],
+
+    rows: [
+      {
+        author: <Provider image={team2} name="John Michael" email="john@creative-tim.com" />,
+        function: <Job title="Manager" description="Organization" />,
+        status: (
+          <MDBox ml={-1}>
+            <MDBadge badgeContent="online" color="success" variant="gradient" size="sm" />
+          </MDBox>
+        ),
+        employed: (
+          <MDTypography component="a" href="#" variant="caption" color="text" fontWeight="medium">
+            23/04/18
+          </MDTypography>
+        ),
+        action: (
+          <MDTypography
+            onClick={handleEdit}
+            component="a"
+            href="#"
+            variant="caption"
+            color="text"
+            fontWeight="medium"
+          >
+            Edit
+          </MDTypography>
+        ),
+      },
+    ],
+  };
+}
