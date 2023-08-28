e58a5a4553403d5e7285ad16345c2699d91769fa
juan <juanxhoo@gmail.com>
2023-08-27 22:54:20 -0500

    changes in update provider component

--- src/layouts/providers/Provider.js
+++ src/layouts/providers/Provider.js
@@ -14,12 +14,11 @@ Coded by www.creative-tim.com
 */
 
 // @mui material components
-import { Card, TableBody, TableContainer, TableRow, Typography } from "@mui/material";
+import { TableBody, TableContainer, TableRow, Typography } from "@mui/material";
 // Material Dashboard 2 React components
 import MDBox from "components/MDBox";
 // Material Dashboard 2 React example components
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
-import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import Table from "@mui/material/Table";
 import TableCell from "@mui/material/TableCell";
 import MDInput from "components/MDInput";
@@ -28,86 +27,82 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import MDButton from "components/MDButton";
 import TextareaAutosize from "@mui/base/TextareaAutosize";
+import InputLabel from "@mui/material/InputLabel";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
 
 function Provider() {
   const { id } = useParams();
+  const [inputValues, setInputValues] = useState({});
+  const [countries, setCountries] = useState([]);
+  const [pwdFocus, setPwdFocus] = useState(false);
+  const [validPwd, setValidPwd] = useState(false);
+  const [emailFocus, setEmailFocus] = useState(false);
+  const [validEmail, setValidEmail] = useState(false);
   const [provider, setProvider] = useState([]);
+  const [cities, setCities] = useState([]);
+  const [changedProvider, setChangedProvider] = useState({});
+  const PWD_REGEX = /^(?=.*\d)(?=.*[a-zA-Z]).{8,}$/;
+  const EMAIL_REGEX =
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  //Layout to show Inputs
-  const rows = [
-    {
-      label: "Name",
-      id: "name",
-      type: "text",
-    },
-    {
-      label: "Last Name",
-      id: "lastname",
-      type: "text",
-    },
-    {
-      label: "Contact",
-      id: "contact",
-      type: "text",
-    },
-    {
-      label: "Address",
-      id: "address",
-      type: "text",
-    },
-    {
-      label: "Phone Number 1",
-      id: "phone1",
-      type: "text",
-    },
-    {
-      label: "Phone Number 2",
-      id: "phone2",
-      type: "text",
-    },
-    {
-      label: "Country",
-      id: "country",
-      type: "text",
-    },
-    {
-      label: "City",
-      id: "city",
-      type: "text",
-    },
-    {
-      label: "Email",
-      id: "email",
-      type: "email",
-    },
-    {
-      label: "Observations",
-      id: "observations",
-      type: "textarea",
-    },
-  ];
+  // Check Password
+  useEffect(() => {
+    setValidPwd(PWD_REGEX.test(inputValues.password));
+  }, [inputValues.password]);
+
+  //Check Email
+  useEffect(() => {
+    setValidEmail(EMAIL_REGEX.test(inputValues.email));
+  }, [inputValues.email]);
 
   useEffect(() => {
-    //Fetching Provider Data from ERP API BACKEND
-    const handleProvider = async (param_id) => {
+
+    //Fetch Provider
+
+    async function fetchProvider() {
       try {
-        const response = await axios.get("http://localhost:3000/v1/users/" + param_id);
+        const response = await axios.get("http://localhost:3000/v1/users/" + id);
         setProvider(response.data);
       } catch (error) {
         console.log(error);
       }
-    };
+    }
+
+
+    //Fetching Countries
+    async function fetchCountries() {
+      try {
+        const response = await axios.get("http://localhost:3000/v1/countries");
+        setCountries(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
     // Call the handleProvider function passing the id parameter
-    handleProvider(id);
+    fetchCountries();
+    fetchProvider();
   }, []);
 
-  const submitChanges = async () => {
+  const fetchCities = async function (countryCode) {
     try {
-      console.log(provider);
-      //const response = await axios.patch("http://localhost:3000/v1/users/" + id, provider);
-      //console.log(response);
+      const response = await axios.get(
+        "http://localhost:3000/v1/countries/" + countryCode + "/cities"
+      );
+      setCities(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-      setProvider(response.data);
+  const submitChanges = async (e) => {
+    e.preventDefault();
+    console.log(inputValues);
+    try {
+      const response = await axios.patch("http://localhost:3000/v1/users/" + id, inputValues, {
+        withCredentials: false,
+      });
+      console.log(response.data);
     } catch (error) {
       console.log(error);
     }
@@ -115,56 +110,295 @@ function Provider() {
 
   return (
     <DashboardLayout>
-      <DashboardNavbar />
       <MDBox>
-        <p>Provider {id}</p>
+        <p>New Provider {id}</p>
         <TableContainer>
-          <MDBox p={4}>
+          <MDBox onSubmit={submitChanges} component="form" p={4}>
             <Table>
               <TableBody>
-                {rows.map((row, index) => (
-                  <TableRow key={index}>
-                    <TableCell>
-                      <Typography sx={{ fontWeight: "bold" }}>{row.label}:</Typography>
-                    </TableCell>
-                    <TableCell>
-                      {row.type === "textarea" ? (
-                        <TextareaAutosize
-                          minRows={7}
-                          value={provider[row.id] || ""}
-                          onChange={(e) => {
-                            setProvider((prevValues) => ({
-                              ...prevValues,
-                              [row.id]: e.target.value,
-                            }));
-                          }}
-                        />
-                      ) : (
-                        <MDInput
-                          variant="outlined"
-                          label={row.label}
-                          value={provider[row.id] || ""}
-                          onChange={(e) => {
-                            setProvider((prevValues) => ({
-                              ...prevValues,
-                              [row.id]: e.target.value,
-                            }));
-                          }}
-                        />
-                      )}
-                    </TableCell>
-                  </TableRow>
-                ))}
+                <TableRow>
+                  <TableCell>
+                    <Typography>Name</Typography>
+                  </TableCell>
+                  <TableCell>
+                    <MDInput
+                     
+                      variant="outlined"
+                      label="name"
+                      value={provider.name || ""}
+                      onChange={(e) => {
+                        setInputValues((prevValues) => ({
+                          ...prevValues,
+                          name: e.target.value,
+                        }));
+                        setProvider((prevValues) => ({
+                          ...prevValues,
+                          name: e.target.value,
+                        }));
+                      }}
+                    ></MDInput>
+                  </TableCell>
+                </TableRow>
+
+                <TableRow>
+                  <TableCell>
+                    <Typography>Lastname</Typography>
+                  </TableCell>
+                  <TableCell>
+                    <MDInput
+                      
+                      variant="outlined"
+                      label="lastname"
+                      value={provider.lastname || ""}
+                      onChange={(e) => {
+                        setInputValues((prevValues) => ({
+                          ...prevValues,
+                          lastname: e.target.value,
+                        }));
+                        setProvider((prevValues) => ({
+                          ...prevValues,
+                          lastname: e.target.value,
+                        }));
+                      }}
+                    ></MDInput>
+                  </TableCell>
+                </TableRow>
+
+                <TableRow>
+                  <TableCell>
+                    <Typography>Contact</Typography>
+                  </TableCell>
+                  <TableCell>
+                    <MDInput
+                      
+                      variant="outlined"
+                      label="Contact"
+                      value={provider.contact || ""}
+                      onChange={(e) => {
+                        setInputValues((prevValues) => ({
+                          ...prevValues,
+                          contact: e.target.value,
+                        }));
+
+                        setProvider((prevValues) => ({
+                          ...prevValues,
+                          contact: e.target.value,
+                        }));
+                      }}
+                    ></MDInput>
+                  </TableCell>
+                </TableRow>
+
+                <TableRow>
+                  <TableCell>
+                    <Typography>Password</Typography>
+                  </TableCell>
+                  <TableCell>
+                    <MDInput
+                      
+                      variant="outlined"
+                      label="Password"
+                      onFocus={() => setPwdFocus(true)}
+                      onBlur={() => setPwdFocus(false)}
+                      onChange={(e) => {
+                        setInputValues((prevValues) => ({
+                          ...prevValues,
+                          password: e.target.value,
+                        }));
+                        setProvider((prevValues) => ({
+                          ...prevValues,
+                          password: e.target.value,
+                        }));
+                      }}
+                    ></MDInput>
+                    <p
+                      id="pwdnote"
+                      className={pwdFocus && !validPwd ? "instructions" : "offscreen"}
+                    >
+                      8 to 24 characters.
+                      <br />
+                      Must include uppercase and lowercase letters, and a number.
+                      <br />
+                    </p>
+                  </TableCell>
+                </TableRow>
+
+                <TableRow>
+                  <TableCell>
+                    <Typography>Address</Typography>
+                  </TableCell>
+                  <TableCell>
+                    <MDInput
+                      
+                      variant="outlined"
+                      label="Address"
+                      value={provider.address || ""}
+                      onChange={(e) => {
+                        setInputValues((prevValues) => ({
+                          ...prevValues,
+                          address: e.target.value,
+                        }));
+                        setProvider((prevValues) => ({
+                          ...prevValues,
+                          address: e.target.value,
+                        }));
+                      }}
+                    ></MDInput>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>
+                    <Typography>Phone</Typography>
+                  </TableCell>
+                  <TableCell>
+                    <MDInput
+                      
+                      variant="outlined"
+                      label="Phone"
+                      value={provider.phone || ""}
+                      onChange={(e) => {
+                        setInputValues((prevValues) => ({
+                          ...prevValues,
+                          phone: e.target.value,
+                        }));
+                        setProvider((prevValues) => ({
+                          ...prevValues,
+                          phone: e.target.value,
+                        }));
+                      }}
+                    ></MDInput>
+                  </TableCell>
+                </TableRow>
+
+                <TableRow>
+                  <TableCell>
+                    <Typography>Email</Typography>
+                  </TableCell>
+                  <TableCell>
+                    <MDInput
+                      onBlur={() => setEmailFocus(false)}
+                      onFocus={() => setEmailFocus(true)}
+                      
+                      variant="outlined"
+                      label="Email"
+                      value={provider.email || ""}
+                      onChange={(e) => {
+                        setInputValues((prevValues) => ({
+                          ...prevValues,
+                          email: e.target.value,
+                        }));
+                        setProvider((prevValues) => ({
+                          ...prevValues,
+                          email: e.target.value,
+                        }));
+                      }}
+                    ></MDInput>
+                    <p
+                      id="emailnote"
+                      className={emailFocus && !validEmail ? "instructions" : "offscreen"}
+                    >
+                      Invalid Email Format.
+                      <br />
+                    </p>
+                  </TableCell>
+                </TableRow>
+
+                <TableRow>
+                  <TableCell>
+                    <Typography>Country</Typography>
+                  </TableCell>
+                  <TableCell>
+                    <InputLabel id="demo-simple-label">Country</InputLabel>
+                    <Select
+                      labelId="country-label"
+                      label="Country"
+                      value={provider.country || ""}
+                      onChange={(e) => {
+                        const selectedCountryName = e.target.value;
+                        const selectedCountry = countries.find(
+                          (country) => country.name === selectedCountryName
+                        );
+
+                        if (selectedCountry) {
+                          setInputValues((prevValues) => ({
+                            ...prevValues,
+                            country: selectedCountry.id,
+                          }));
+                          setProvider((prevValues) => ({
+                            ...prevValues,
+                            country: e.target.value,
+                          }));
+                          fetchCities(selectedCountry.code); // Pass the country code to fetchCities
+                        }
+                      }}
+                    >
+                      {countries.map((country) => (
+                        <MenuItem key={country.id} value={country.name}>
+                          {country.name}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </TableCell>
+                </TableRow>
+
+                <TableRow>
+                  <TableCell>
+                    <Typography>City:</Typography>
+                  </TableCell>
+                  <TableCell>
+                    <Select
+                      value={provider.city || ""}
+                      onChange={(e) => {
+                        setInputValues((prevValues) => ({
+                          ...prevValues,
+                          city: e.target.value,
+                        }));
+                        setProvider((prevValues) => ({
+                          ...prevValues,
+                          city: e.target.value,
+                        }));
+                      }}
+                    >
+                      {cities.map((city) => (
+                        <MenuItem key={city.id} value={city.name}>
+                          {city.name}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </TableCell>
+                </TableRow>
+
+                <TableRow>
+                  <TableCell>
+                    <Typography>Observations:</Typography>
+                  </TableCell>
+                  <TableCell>
+                    <TextareaAutosize
+                      minRows={7}
+                      value={provider.observations || ""}
+                      onChange={(e) => {
+                        setInputValues((prevValues) => ({
+                          ...prevValues,
+                          observations: e.target.value,
+                        }));
+                        setProvider((prevValues) => ({
+                          ...prevValues,
+                          observations: e.target.value,
+                        }));
+                      }}
+                    />
+                  </TableCell>
+                </TableRow>
               </TableBody>
             </Table>
             <MDButton
+              type="submit"
               variant="gradient"
               color="info"
-              onClick={submitChanges}
               p={4}
               sx={{ margin: "20px", float: "right" }}
             >
-              Submit Changes
+              Update Provider
             </MDButton>
           </MDBox>
         </TableContainer>
@@ -173,4 +407,56 @@ function Provider() {
   );
 }
 
+
 export default Provider;
+
+// <TableCell>
+// {row.type === "textarea" ? (
+//   <TextareaAutosize
+//     minRows={7}
+//     value={provider[row.id] || ""}
+//     onChange={(e) => {
+//       setProvider((prevValues) => ({
+//         ...prevValues,
+//         [row.id]: e.target.value,
+//       }));
+//       setChangedProvider((prevValues) => ({
+//         ...prevValues,
+//         [row.id]: e.target.value,
+//       }));
+//     }}
+//   />
+// ) : row.type === "select" ? (
+//   <Select
+//     value={provider[row.id] || ""}
+//     onChange={(e) => {
+//       setProvider((prevValues) => ({
+//         ...prevValues,
+//         [row.id]: e.target.value,
+//       }));
+//       setChangedProvider((prevValues) => ({
+//         ...prevValues,
+//         [row.id]: e.target.value,
+//       }));
+//     }}
+//   >
+//     {/* Render your select options here */}
+//   </Select>
+// ) : (
+//   <MDInput
+//     variant="outlined"
+//     label={row.label}
+//     value={provider[row.id] || ""}
+//     onChange={(e) => {
+//       setProvider((prevValues) => ({
+//         ...prevValues,
+//         [row.id]: e.target.value,
+//       }));
+//       setChangedProvider((prevValues) => ({
+//         ...prevValues,
+//         [row.id]: e.target.value,
+//       }));
+//     }}
+//   />
+// )}
+// </TableCell>
